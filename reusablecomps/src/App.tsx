6e545eb3bf2308f6d6867dd07607be98df91e06d
neomarino333 <61431560+neomarino333@users.noreply.github.com>
2022-10-23 12:31:20 -0400

    Add input files & Comboboxes, add headless ui

--- reusablecomps/src/App.tsx
+++ reusablecomps/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1 className="text-3xl font-bold underline text-red-400">
+      <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
         <img src={logo} className="App-logo" alt="logo" />
@@ -22,6 +23,7 @@ function App() {
           Learn React
         </a>
       </header>
+        <CheckIcon className='w-10 h-10 text-orange-400'/>
     </div>
   );
 }
