dcbb83a0f5700e8cc46422be9ce29ef7fea28b16
neomarino333 <61431560+neomarino333@users.noreply.github.com>
2022-10-23 20:41:53 -0400

    added combobox

--- reusablecomps/src/Reusables/Combobox.tsx
+++ reusablecomps/src/Reusables/Combobox.tsx
@@ -1,108 +1,152 @@
-// import React from 'react'
-// import { Fragment, useState } from 'react'
-// import { Combobox, Transition } from '@headlessui/react'
-// import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
+import * as React from 'react'
+import { useState } from 'react'
+import { Combobox, Transition } from '@headlessui/react'
+import { CheckIcon, ArrowsUpDownIcon } from '@heroicons/react/24/solid'
 
-// const people = [
-//   { id: 1, name: 'Wade Cooper' },
-//   { id: 2, name: 'Arlene Mccoy' },
-//   { id: 3, name: 'Devon Webb' },
-//   { id: 4, name: 'Tom Cook' },
-//   { id: 5, name: 'Tanya Fox' },
-//   { id: 6, name: 'Hellen Schmidt' },
-// ]
+function classNames(...classes: string[]): string {
+    return classes.filter(Boolean).join(' ')
+  }
 
-// export default function Example() {
-//   const [selected, setSelected] = useState(people[0])
-//   const [query, setQuery] = useState('')
-
-//   const filteredPeople =
-//     query === ''
-//       ? people
-//       : people.filter((person) =>
-//           person.name
-//             .toLowerCase()
-//             .replace(/\s+/g, '')
-//             .includes(query.toLowerCase().replace(/\s+/g, ''))
-//         )
-
-//   return (
-//     <div className="fixed top-16 w-72">
-//       <Combobox value={selected} onChange={setSelected}>
-//         <div className="relative mt-1">
-//           <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
-//             <Combobox.Input
-//               className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
-//               displayValue={(person) => person.name}
-//               onChange={(event) => setQuery(event.target.value)}
-//             />
-//             <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
-//               <ChevronUpDownIcon
-//                 className="h-5 w-5 text-gray-400"
-//                 aria-hidden="true"
-//               />
-//             </Combobox.Button>
-//           </div>
-//           <Transition
-//             as={Fragment}
-//             leave="transition ease-in duration-100"
-//             leaveFrom="opacity-100"
-//             leaveTo="opacity-0"
-//             afterLeave={() => setQuery('')}
-//           >
-//             <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-//               {filteredPeople.length === 0 && query !== '' ? (
-//                 <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
-//                   Nothing found.
-//                 </div>
-//               ) : (
-//                 filteredPeople.map((person) => (
-//                   <Combobox.Option
-//                     key={person.id}
-//                     className={({ active }) =>
-//                       `relative cursor-default select-none py-2 pl-10 pr-4 ${
-//                         active ? 'bg-teal-600 text-white' : 'text-gray-900'
-//                       }`
-//                     }
-//                     value={person}
-//                   >
-//                     {({ selected, active }) => (
-//                       <>
-//                         <span
-//                           className={`block truncate ${
-//                             selected ? 'font-medium' : 'font-normal'
-//                           }`}
-//                         >
-//                           {person.name}
-//                         </span>
-//                         {selected ? (
-//                           <span
-//                             className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
-//                               active ? 'text-white' : 'text-teal-600'
-//                             }`}
-//                           >
-//                             <CheckIcon className="h-5 w-5" aria-hidden="true" />
-//                           </span>
-//                         ) : null}
-//                       </>
-//                     )}
-//                   </Combobox.Option>
-//                 ))
-//               )}
-//             </Combobox.Options>
-//           </Transition>
-//         </div>
-//       </Combobox>
-//     </div>
-//   )
-// }
-
-import React from 'react'
-
-const Combobox = () => {
-  return (
-    <div>Combobox</div>
-  )
+type Option = {
+  id: string
+  name: string
+  description: string
 }
 
-export default Combobox
+interface IProps {
+  onChange: (value: Option) => void
+  options: Array<Option>
+  label?: string
+  value: Option
+  error?: string
+  placeholder?: string
+  autoFocus?: boolean
+  open: boolean,
+  dimensions?: {
+    height: number
+    width: number
+  }
+}
+export const ComboboxHUI = ({
+  options,
+  value,
+  onChange,
+  error,
+  placeholder,
+  autoFocus,
+}: IProps) => {
+  const [query, setQuery] = useState('')
+  const [selected, setSelected] = useState(value || ({} as Option))
+
+  //this is needed for when outside state sets the value
+  React.useEffect(() => {
+    if (value) {
+      return setSelected(value)
+    }
+  }, options)
+
+  if (!options?.length) {
+    return null
+  }
+
+  const filteredOption =
+    query === ''
+      ? options
+      : options.filter((option) =>
+          option.name
+            .toLowerCase()
+            .replace(/\s+/g, '')
+            .includes(query.toLowerCase().replace(/\s+/g, '')),
+        )
+
+  return (
+    <Combobox
+      placeholder='Select an option'
+      value={selected}
+      onChange={(value: Option) => {
+        onChange(value)
+        setSelected(value)
+      }}
+    >
+      {({ open }) => (
+        <div className={`relative w-[300px]`}>
+          <Combobox.Input
+            className={classNames(
+              error
+                ? 'text-red-500 border-red-500 focus:ring-red-500'
+                : ' border-first-grey-border text-first-blue focus:ring-first-blue focus:border-first-blue',
+              'shadow-sm block sm:text-sm rounded-md h-[34px] w-[300px]',
+            )}
+            placeholder={placeholder}
+            onChange={(event) => {
+              setQuery(event.target.value)
+            }}
+            value={query}
+            displayValue={(option: { id: string; name: string }) => option.name}
+            autoComplete='my-field-name1'
+            autoFocus={autoFocus}
+          />
+          <Combobox.Button className='absolute inset-y-0 right-0 flex items-center pr-2'>
+            <ArrowsUpDownIcon
+              className='w-5 h-5 text-gray-400'
+              aria-hidden='true'
+            />
+          </Combobox.Button>
+
+          <Transition
+            show={open}
+            leave='transition duration-100 ease-in'
+            leaveFrom='transform opacity-100'
+            afterLeave={() => setQuery('')}
+          >
+            <Combobox.Options className='absolute w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm z-[99999]'>
+              {filteredOption.length === 0 && query !== '' ? (
+                <div className='relative px-4 py-2 cursor-default select-none text-first-blue'>
+                  Nothing found.
+                </div>
+              ) : (
+                filteredOption.map((option) => (
+                  <Combobox.Option
+                    key={option.id}
+                    className={({ active }) =>
+                      `relative cursor-pointer select-none py-2 pl-5 pr-4 ${
+                        active ? 'bg-first-blue text-white' : 'text-first-blue'
+                      }`
+                    }
+                    value={option}
+                  >
+                    <span
+                      className={`block truncate ${
+                        selected.id === option.id
+                          ? 'font-medium'
+                          : 'font-normal'
+                      }`}
+                    >
+                      {option.name}
+                      <div
+                        className={`block truncate ${
+                          selected.id === option.id
+                            ? 'text-xs font-medium'
+                            : 'text-xs font-normal'
+                        }`}
+                      >
+                        {option.description}
+                      </div>
+                    </span>
+                    {selected.id === option.id ? (
+                      <span
+                        className={`absolute inset-y-0 flex items-center right-0 pr-4`}
+                      >
+                        <CheckIcon className='w-5 h-5 ' aria-hidden='true' />
+                      </span>
+                    ) : null}
+                  </Combobox.Option>
+                ))
+              )}
+            </Combobox.Options>
+          </Transition>
+        </div>
+      )}
+    </Combobox>
+  )
+}
